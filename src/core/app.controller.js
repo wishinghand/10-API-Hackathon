05f59f0c4cb1022e35194a58c021e7c6fb6931e8
Ryan Dowdy <rdowdy21@gmail.com>
2016-08-19 14:13:59 -0700

    Fix Angular Google Maps plugin

--- src/core/app.controller.js
+++ src/core/app.controller.js
@@ -23,7 +23,7 @@
         vm.shuffleHeadings = shuffleHeadings;
 
         // new config object, defaulted to san diego coordinates.
-        vm.map = { center: { latitude: -32.7157, longitude: 117.1611 }, zoom: 15 };
+        vm.map = { center: { latitude: 32.7157, longitude: -117.1611 }, zoom: 15 };
 
         vm.fun_types = ['amusement_park', 'aquarium', 'bar', 'art_gallery', 'book_store', 'campground', 'casino', 'beauty_salon', 'florist', 'movie_rental', 'movie_theater', 'museum', 'night_club', 'park', 'restaurant', 'shopping_mall', 'spa', 'stadium', 'zoo'];
         vm.serious_types = ['airport', 'atm', 'bank', 'car_repair', 'city_hall', 'courthouse', 'dentist', 'doctor', 'electrician', 'embassy', 'fire_station', 'gas_station', 'grocery_or_supermarket', 'gym', 'hardware_store', 'hospital', 'laundry', 'lawyer', 'library', 'locksmith', 'local_government_office', 'lodging', 'painter', 'parking', 'pharmacy', 'plumber', 'police', 'post_office', 'storage', 'train_station', 'transit_station', 'veterinary_care'];
@@ -75,6 +75,7 @@
 
         uiGmapGoogleMapApi.then(function(maps) {
             GoogleAPIFactory.setMapObject(maps);
+            vm.loading = false;
         });
     }
 })();
