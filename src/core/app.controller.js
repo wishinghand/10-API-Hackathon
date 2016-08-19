d6d7a941df7ad58362985175682fe0c14d486c88
Ryan Dowdy <rdowdy21@gmail.com>
2016-08-19 14:21:26 -0700

    Fixed map options

--- src/core/app.controller.js
+++ src/core/app.controller.js
@@ -16,7 +16,19 @@
         //this function sets the map on page load
         vm.setMap = setMap;
         vm.coordinates = [];
-        vm.map = { center: { latitude: 32.7157, longitude: -117.1611 }, zoom: 15 };
+        vm.map = {
+            center: { latitude: 32.7157, longitude: -117.1611 },
+            zoom: 15,
+            options: {
+                styles: [{
+                    stylers: [{ visibility: 'simplified' }]
+                }, {
+                    elementType: 'labels',
+                    stylers: [{ visibility: 'off' }]
+                }],
+                scrollwheel: false
+            }
+        };
         vm.title = 'appCtrl';
         vm.fun_types = ['amusement_park', 'aquarium', 'bar', 'art_gallery', 'book_store', 'campground', 'casino', 'beauty_salon', 'florist', 'movie_rental', 'movie_theater', 'museum', 'night_club', 'park', 'restaurant', 'shopping_mall', 'spa', 'stadium', 'zoo'];
         vm.serious_types = ['airport', 'atm', 'bank', 'car_repair', 'city_hall', 'courthouse', 'dentist', 'doctor', 'electrician', 'embassy', 'fire_station', 'gas_station', 'grocery_or_supermarket', 'gym', 'hardware_store', 'hospital', 'laundry', 'lawyer', 'library', 'locksmith', 'local_government_office', 'lodging', 'painter', 'parking', 'pharmacy', 'plumber', 'police', 'post_office', 'storage', 'train_station', 'transit_station', 'veterinary_care'];
