b9657b10917a85bbc376b1ad12c994f2271ebfa8
wishinghand <wishinghand@gmail.com>
2016-08-19 09:55:19 -0700

    trying to add google maps angular module

---
+++ src/core/maps.config.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(function(uiGmapGoogleMapApiProvider) {
+            uiGmapGoogleMapApiProvider.configure({
+                //    key: 'your api key',
+                v: '3.20', //defaults to latest 3.X anyhow
+                libraries: 'weather,geometry,visualization'
+            });
+        })
+})();
