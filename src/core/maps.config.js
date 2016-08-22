d22507774a08c5ecb9a62a90a9b34cfcc345f8be
wishinghand <wishinghand@gmail.com>
2016-08-22 10:30:45 -0700

    api key fuckery

--- src/core/maps.config.js
+++ src/core/maps.config.js
@@ -3,9 +3,9 @@
 
     angular
         .module('app')
-        .config(function(uiGmapGoogleMapApiProvider) {
+        .config(function(uiGmapGoogleMapApiProvider, apiKey) {
             uiGmapGoogleMapApiProvider.configure({
-                //    key: 'your api key',
+                key: apiKey.googlePlaces,
                 v: '3.20', //defaults to latest 3.X anyhow
                 libraries: 'weather,geometry,visualization'
             });
