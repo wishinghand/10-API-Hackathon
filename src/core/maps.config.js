0a90a2992ccacbbb3aa35c26698d5e7d356fca6a
Ryan Wilson <comrww@gmail.com>
2016-10-11 12:50:32 -0700

    initial commit

--- src/core/maps.config.js
+++ src/core/maps.config.js
@@ -5,6 +5,7 @@
         .module('app')
         .config(function(uiGmapGoogleMapApiProvider) {
             uiGmapGoogleMapApiProvider.configure({
+                // key: apiKey.googlePlaces,
                 v: '3.20', //defaults to latest 3.X anyhow
                 libraries: 'weather,geometry,visualization'
             });
